b5fbd9ba8422668dc6117032a97e5f5ff3241b10
Andrey Petrov <andrey.petrov@shazow.net>
2024-03-06 13:10:27 -0500

    pnpm upgrade --latest

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess()],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
