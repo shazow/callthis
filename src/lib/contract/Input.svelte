bca0ef2646b9a8623286318d4a2389fe210d8fd8
Andrey Petrov <andrey.petrov@shazow.net>
2024-03-23 15:01:37 -0600

    WIP: contract/Input.svelte: Expand/shrink arrays;

--- src/lib/contract/Input.svelte
+++ src/lib/contract/Input.svelte
@@ -20,21 +20,44 @@
     return param;
   }
 
+  function shrinkArray(param: Param): Param {
+    if (!param.children) throw new Error("Param.shrinkArray: must have children");
+    param.children.pop();
+    return param;
+  }
+
 </script>
 
-<section class="input">
+<details open class="input">
+  <summary>{input.type.name} ({input.type.baseType})</summary>
   {#if input.children }
     {#each input.children as p}
       <svelte:self input={ p } componentArgs={ componentArgs } />
     {/each}
+
     {#if input.childrenExtendType}
-      <button on:click|preventDefault={ () => { input = extendArray(input) } }>+</button> <span>{input.childrenExtendType.type.baseType}</span>a
+      <button on:click|preventDefault={ () => { input = extendArray(input) } }>+</button>
+      <button on:click|preventDefault={ () => { input = shrinkArray(input) } } disabled={ !input.children.length }>-</button>
+      <span>{input.childrenExtendType.type.format()}</span>
     {/if}
+
   {:else if input.type.baseType === "address"}
     <Address required bind:value={ input.value } bind:resolved={ input.resolved } {...componentArgs}><span>{input.type.name}</span></Address>
+
   {:else}
-    <span class="input-name">{input.type.name}</span>
     <input type="text" required bind:value={ input.value } {...componentArgs} />
-    <aside>{input.type}</aside>
+    <aside>{input.type.baseType}</aside>
   {/if}
-</section>
+</details>
+
+<style lang="scss">
+details {
+  margin-left: 1em;
+  margin-bottom: 0.5em;
+  border-left: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+button {
+  width: initial;
+}
+</style>
