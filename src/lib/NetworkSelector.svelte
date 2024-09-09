18488d2a5290de16a5fad855a5b1bc3a0e3c8c19
Andrey Petrov <andrey.petrov@shazow.net>
2024-09-09 14:38:30 -0700

    src/lib/NetworkSelector.svelte: injectedNetwork absorbs unavailable selected value

--- src/lib/NetworkSelector.svelte
+++ src/lib/NetworkSelector.svelte
@@ -8,9 +8,10 @@
     rpc: string[];
   };
 
+  const injectedNetwork = { chainid: 0, name: "↪️ Injected Provider via Connected Wallet", rpc: [], }
+
   // Get more from here https://chainlist.org/
   export let networks : Network[] = [
-      { chainid: 0, name: "↪️ Injected Provider via Connected Wallet", rpc: [], },
       { chainid: 1, name: "Ethereum Mainnet", rpc: ["https://eth.llamarpc.com", "https://ethereum-rpc.publicnode.com"] },
       { chainid: 10, name: "OP Mainnet (Optimism)", rpc: ["https://optimism.llamarpc.com", "https://optimism-rpc.publicnode.com"] },
       { chainid: 56, name: "Binance Smart Chain", rpc: ["https://bsc.llamarpc.com"] },
@@ -30,22 +31,19 @@
     // `selected` is updated after the event is emitted, so can't reliably use it here.
     const target = event.target as HTMLSelectElement;
     const chainid = Number(target.value);
-    dispatch("change", { network: networks.find(n => n.chainid === chainid) ?? networks[0] });
+    dispatch("change", { network: networks.find(n => n.chainid === chainid) ?? injectedNetwork });
   };
 
   export function change(chainid: number) {
     if (chainid === selected) return;
     selected = chainid;
-    dispatch("change", { network: networks.find(n => n.chainid === chainid) ?? networks[0] });
+    dispatch("change", { network: networks.find(n => n.chainid === chainid) ?? injectedNetwork });
   }
 </script>
 
-{#if available.has(selected)}
 <select on:change={onChange} bind:value={selected} {disabled}>
+  <option value={available.has(selected) ? 0 : selected}>{injectedNetwork.name}</option>
   {#each networks as network (network.chainid)}
     <option value={network.chainid} selected={network.chainid === selected}>{network.name}</option>
   {/each}
 </select>
-{:else}
-<select disabled={true}><option selected={true}>{networks[0].name}</option></select>
-{/if}
