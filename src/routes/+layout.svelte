b27ccb2ba2cce678d608106fa2a36faeb5bc17bc
Andrey Petrov <andrey.petrov@shazow.net>
2025-02-09 21:26:57 -0800

    Add built for ethereum

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,13 +9,20 @@
 </main>
 
 <footer>
-  <a target="_blank" href="https://github.com/shazow/callthis">
-    <OpenSourceIcon /> <span>Open Source</span>
-  </a>
+  <div><a href="/" style="color: var(--header-text-color);">callthis.eth</a></div>
+  <div>
+    <a target="_blank" href="https://github.com/shazow/callthis">
+      <OpenSourceIcon /> <span>Open Source</span>
+    </a>
+  </div>
+  <div>
+    <EthereumIcon /> <span>Built for Ethereum</span>
+  </div>
 </footer>
 
 <script lang="ts">
   import "../style.scss";
   import OpenSourceIcon from "../lib/icons/openSource.svelte"
+  import EthereumIcon from "../lib/icons/ethereum.svelte"
   import ThemeButton from "$lib/ThemeButton.svelte";
 </script>
